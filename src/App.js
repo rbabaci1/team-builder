326d7ab120a68c853e04a8af991af08158e4cca2
rabah babaci <4rabah@gmail.com>
2020-02-17 21:42:50 -0800

    import MemberCard component to App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import "./App.css";
 import membersData from "./data/membersData";
+import MemberCard from "./Components/MemberCard/MemberCard";
 
 function App() {
   return <div className="App"></div>;
