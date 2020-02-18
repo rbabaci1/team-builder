65537f6b6843fbeb4c78ffe9c88dd0386a6f04e7
rabah babaci <4rabah@gmail.com>
2020-02-17 21:45:00 -0800

    set state to the members data

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import MemberCard from "./Components/MemberCard/MemberCard";
 import Form from "./Components/Form/Form";
 
 function App() {
+  const [teamData, setTeamData] = useState(membersData);
+
   return <div className="App"></div>;
 }
 
