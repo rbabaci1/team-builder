c625b6e6217630ab2cbf57048a9557a3ab1835f1
rabah babaci <4rabah@gmail.com>
2020-02-18 14:23:30 -0800

    remove the data file and intialize the data stata with an array with one obj

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
 
 import "./App.css";
-import membersData from "./data/membersData";
 import MemberCard from "./Components/MemberCard/MemberCard";
 import Form from "./Components/Form/Form";
 
 function App() {
-  const [teamData, setTeamData] = useState(membersData);
+  const [teamData, setTeamData] = useState([
+    {
+      firstName: "Rabah",
+      lastName: "Babaci",
+      age: 25,
+      gender: "Male",
+      email: "4rabah@gmail.com",
+      phoneNumber: "510-646-7743",
+      role: "Full-Stack Web Developer",
+      isHappy: "Yes"
+    }
+  ]);
   const [formInput, setFormInput] = useState({
     firstName: "",
     lastName: "",
@@ -22,7 +32,7 @@ function App() {
     <div className="App">
       <Form />
       <div className="card-wrapper">
-        {membersData.map((member, index) => (
+        {teamData.map((member, index) => (
           <MemberCard member={member} index={index} />
         ))}
       </div>
