bba99cfb876ddc9ec3ead664ef488ab5ea7c1e07
rabah babaci <4rabah@gmail.com>
2020-02-17 21:43:19 -0800

    import Form component to App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import "./App.css";
 import membersData from "./data/membersData";
 import MemberCard from "./Components/MemberCard/MemberCard";
+import Form from "./Components/Form/Form";
 
 function App() {
   return <div className="App"></div>;
