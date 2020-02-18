73e0d074fe156549489c6a681b7f5021dad47330
rabah babaci <4rabah@gmail.com>
2020-02-17 21:51:07 -0800

    add style to App and memberCard

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import "./App.css";
 import membersData from "./data/membersData";
 import MemberCard from "./Components/MemberCard/MemberCard";
-import Form from "./Components/Form/Form";
+// import Form from "./Components/Form/Form";
 
 function App() {
   const [teamData, setTeamData] = useState(membersData);
--- src/Components/MemberCard/MemberCard.js
+++ src/Components/MemberCard/MemberCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./memberCard.css";
 
 export default function MemberCard({ member, index }) {
   return (
