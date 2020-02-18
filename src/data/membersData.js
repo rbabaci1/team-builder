b51c32232a8040f744ae4a485fab7b0be786f8f6
rabah babaci <4rabah@gmail.com>
2020-02-17 21:38:06 -0800

    create members mock data

---
+++ src/data/membersData.js
@@ -0,0 +1,24 @@
+const membersData = [
+  {
+    firstName: "Rabah",
+    lastName: "Babaci",
+    age: 25,
+    gender: "Male",
+    email: "4rabah@gmail.com",
+    phoneNumber: "510-646-7743",
+    role: "Full-Stack Web Developer",
+    isHappy: "Yes"
+  },
+  {
+    firstName: "Kyla",
+    lastName: "Gifford",
+    age: 22,
+    gender: "Female",
+    email: "test@gmail.com",
+    phoneNumber: "510-000-0000",
+    role: "Web Designer",
+    isHappy: "Yes"
+  }
+];
+
+export default membersData;
