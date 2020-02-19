5387dfc51f551334c444b9f24bf4e188e896685c
rabah babaci <4rabah@gmail.com>
2020-02-18 17:29:46 -0800

    create a state for the member to edit in App

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
       isHappy: true
     }
   ]);
+  const [memberToEdit, setMemberToEdit] = useState([]);
 
   const addMember = member => setMembersList([...membersList, member]);
 
